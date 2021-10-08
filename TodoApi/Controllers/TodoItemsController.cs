5701984831abccbe80918fe49c313a2f0cf5f326
veikkahonkanen <veikka.honkanen@gmail.com>
2021-10-08 15:03:45 +0300

    Restored deleted todo-related files and crew model as commented files

---
+++ TodoApi/Controllers/TodoItemsController.cs
@@ -0,0 +1,131 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
+namespace TodoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoItemsController : ControllerBase
+    {
+        private readonly TodoContext _context;
+
+        public TodoItemsController(TodoContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/TodoItems
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoItems()
+        {
+            return await _context.TodoItems.ToListAsync();
+        }
+
+        Func<TodoItem, bool> todoItemComparer; // Delegaatti
+        static bool TrueComparism(TodoItem item)
+        {
+            return item.IsComplete == true;
+        }
+        static bool FalseComparism(TodoItem item)
+        {
+            return item.IsComplete == false;
+        }
+
+        // GET: api/GetAllTrueTodoItems
+        [HttpGet("api/GetAllTrueTodoItems")]
+        public async Task<ActionResult<IEnumerable<TodoItem>>> GetTrueTodoItems()
+        {
+            // todoItemComparer = FalseComparism;
+            todoItemComparer = TrueComparism; // Vaihtoehtoinen ja lyhyempi tapa on anonyymifunktio
+
+            var trueTodoItems =  _context.TodoItems.Where(todoItemComparer).ToArray(); // ToArray ei muokata, ja on nopeampi. ToList muokataan jotenkin
+
+            return trueTodoItems;
+        }
+
+        // GET: api/TodoItems/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TodoItem>> GetTodoItem(long id)
+        {
+            var todoItem = await _context.TodoItems.FindAsync(id);
+
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            return todoItem;
+        }
+
+        // PUT: api/TodoItems/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTodoItem(long id, TodoItem todoItem)
+        {
+            if (id != todoItem.id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(todoItem).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TodoItemExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/TodoItems
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem todoItem)
+        {
+            _context.TodoItems.Add(todoItem);
+            await _context.SaveChangesAsync();
+
+            // return CreatedAtAction("GetTodoItem", new { id = todoItem.id }, todoItem);
+            return CreatedAtAction(nameof(GetTodoItem), new { id = todoItem.id }, todoItem);
+        }
+
+        // DELETE: api/TodoItems/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTodoItem(long id)
+        {
+            var todoItem = await _context.TodoItems.FindAsync(id);
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.TodoItems.Remove(todoItem);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TodoItemExists(long id)
+        {
+            return _context.TodoItems.Any(e => e.id == id);
+        }
+    }
+}
+*/
